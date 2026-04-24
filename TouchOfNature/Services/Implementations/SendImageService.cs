a599394c902f850fdf9d14147d257651e1850420
Fares <fareseldagen1@gmail.com>
2026-04-24 20:10:05 +0300

    Remove Dashboard and refactor Greenhouse service
    - Deleted the frontend signal r test
    - Updated `SendImageService` to use `IConfiguration` for dynamic prediction URL configuration.

--- TouchOfNature/Services/Implementations/SendImageService.cs
+++ TouchOfNature/Services/Implementations/SendImageService.cs
@@ -2,16 +2,19 @@
 using System.Net.Http.Headers;
 using TouchOfNature.Services.Interfaces;
 using static System.Net.Mime.MediaTypeNames;
+using Microsoft.Extensions.Configuration;
 
 namespace TouchOfNature.Services.Implementations
 {
     public class SendImageService : ISendImageService
     {
          private readonly HttpClient _client;
+         private readonly IConfiguration _configuration;
 
-        public SendImageService(HttpClient client)
+        public SendImageService(HttpClient client, IConfiguration configuration)
         {
             _client = client;
+            _configuration = configuration;
         }
 
         public async Task<string> SendImageAsync(IFormFile image)
@@ -25,7 +28,9 @@ namespace TouchOfNature.Services.Implementations
             fileContent.Headers.ContentType = new MediaTypeHeaderValue(image.ContentType);
 
             content.Add(fileContent, "file", image.FileName);
-            var response = await _client.PostAsync("https://rise-elevator-passage.ngrok-free.dev/predict", content);
+            
+            var predictUrl = _configuration["PredictUrl"];
+            var response = await _client.PostAsync(predictUrl, content);
 
             return await response.Content.ReadAsStringAsync();
 
